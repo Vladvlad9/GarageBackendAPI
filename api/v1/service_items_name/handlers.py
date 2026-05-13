aa5d91950a38f03ef023a3b1109551014934ef6d
Vlad Pavlechko <vlad9.ru@gmail.com>
2026-05-13 15:25:41 +0300

    add responses in router

--- api/v1/service_items_name/handlers.py
+++ api/v1/service_items_name/handlers.py
@@ -1,10 +1,21 @@
 from fastapi import APIRouter
 from starlette import status
-
+from starlette.status import (
+    HTTP_404_NOT_FOUND,
+    HTTP_500_INTERNAL_SERVER_ERROR,
+    HTTP_429_TOO_MANY_REQUESTS,
+    HTTP_409_CONFLICT
+)
 from api.annotated_types import PageQuery, PageSizeQuery
 from api.annotated_types.service_item_name import ItemNameID
 from api.dependencies.services import ItemNameServiceDepends
 from api.dependencies.services.auth import AuthenticateHeaderDepends
+from src.types.exeptions import (
+    ObjectNotFoundErrorDTO,
+    HTTPExceptionErrorDTO,
+    ToManyRequestsErrorDTO,
+    ObjectAlreadyExistErrorDTO
+)
 from src.types.pagination import Paginator
 from src.types.service_item_name import ServiceItemNameBase, ItemNameCreateDTO, ItemNameUpdateDTO
 
@@ -14,7 +25,11 @@ router = APIRouter(tags=["ServiceItemsName"], dependencies=[AuthenticateHeaderDe
 @router.get(
     path="/",
     status_code=status.HTTP_200_OK,
-    response_model=Paginator[ServiceItemNameBase]
+    response_model=Paginator[ServiceItemNameBase],
+    responses={
+        HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPExceptionErrorDTO},
+        HTTP_429_TOO_MANY_REQUESTS: {"model": ToManyRequestsErrorDTO},
+    },
 )
 async def get_service_car_list(
         service: ItemNameServiceDepends,
@@ -27,7 +42,12 @@ async def get_service_car_list(
 @router.get(
     path="/{id}",
     status_code=status.HTTP_200_OK,
-    response_model=ServiceItemNameBase
+    response_model=ServiceItemNameBase,
+    responses={
+        HTTP_404_NOT_FOUND: {"model": ObjectNotFoundErrorDTO},
+        HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPExceptionErrorDTO},
+        HTTP_429_TOO_MANY_REQUESTS: {"model": ToManyRequestsErrorDTO},
+    },
 )
 async def get_service_items_name(
         service: ItemNameServiceDepends,
@@ -39,7 +59,12 @@ async def get_service_items_name(
 @router.post(
     path="/{id}",
     status_code=status.HTTP_201_CREATED,
-    response_model=ServiceItemNameBase
+    response_model=ServiceItemNameBase,
+    responses={
+        HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPExceptionErrorDTO},
+        HTTP_409_CONFLICT: {"model": ObjectAlreadyExistErrorDTO},
+        HTTP_429_TOO_MANY_REQUESTS: {"model": ToManyRequestsErrorDTO},
+    }
 )
 async def create_service_items_name(
         data: ItemNameCreateDTO,
@@ -51,7 +76,12 @@ async def create_service_items_name(
 @router.patch(
     path="/{id}",
     status_code=status.HTTP_202_ACCEPTED,
-    response_model=ServiceItemNameBase
+    response_model=ServiceItemNameBase,
+    responses={
+        HTTP_404_NOT_FOUND: {"model": ObjectNotFoundErrorDTO},
+        HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPExceptionErrorDTO},
+        HTTP_429_TOO_MANY_REQUESTS: {"model": ToManyRequestsErrorDTO},
+    },
 )
 async def update_service_items_name(
         item_id: ItemNameID,
@@ -64,6 +94,11 @@ async def update_service_items_name(
 @router.delete(
     path="/{id}",
     status_code=status.HTTP_204_NO_CONTENT,
+    responses={
+        HTTP_404_NOT_FOUND: {"model": ObjectNotFoundErrorDTO},
+        HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPExceptionErrorDTO},
+        HTTP_429_TOO_MANY_REQUESTS: {"model": ToManyRequestsErrorDTO},
+    },
 )
 async def delete_service_items_name(
         item_id: ItemNameID,
